2212b3673dab24ee766004aab6bf8d3d6b7df9ad
varsha <adhikarivarsha55@gmail.com>
2021-07-03 12:41:43 -0700

    url

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import home
+from .views import *
 
 app_name="home"
 urlpatterns = [
-    path('', home, name='home')
+    path('index', home, name='home'),
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
+    path('portfolio', portfolio, name='portfolio'),
+    path('price', price, name='price'),
+    path('services', services, name='services'),
+
+
     ]
+ 
--- home/views.py
+++ home/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 def home(request):
 
 	return render(request,'index.html')
+
+def about(request):
+
+	return render(request,'about.html')
+
+def contact(request):
+
+	return render(request,'contact.html')
+
+def portfolio(request):
+
+	return render(request,'portfolio.html')
+
+def price(request):
+
+	return render(request,'price.html')
+
+def services(request):
+
+	return render(request,'services.html')
